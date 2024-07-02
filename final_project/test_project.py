563d72d236106b111bb4b0727f1ca2279ebe3d29
Grasjusz <46102483+Grasjusz@users.noreply.github.com>
2024-07-02 12:04:17 +0000

    update

--- final_project/test_project.py
+++ final_project/test_project.py
@@ -11,7 +11,10 @@ from lang_engine import (
 def test():
     test_exit()
     test_check_language()
-
+    test_check_language_exit()
+    test_check_language_error()
+    test_chosen_lang()
+    
 def test_exit():    
     with pytest.raises(SystemExit) as exitprogram:
         exit_program()
@@ -22,5 +25,19 @@ def test_check_language():
     assert check_language("polski") == 'Jakiego języka chcesz się uczyć? angielski lub niemiecki?: '
     assert check_language("english") == 'Which language would you like to learn? polish or german?: '
     assert check_language("deutsch") == 'Welches Sprache möchtest du lernen? polnisch oder englisch?: '
+
+def test_check_language_exit():
+    with pytest.raises(SystemExit) as exitprogram:
+        exit_program()
+    assert exitprogram.type == SystemExit    
+    assert exitprogram.value.code == 0
+
+def test_check_language_error():
     assert check_language(" ") == 'Try again, press any key...'
+    assert check_language(" 123 ") == 'Try again, press any key...'
+    assert check_language("test") == 'Try again, press any key...'
     
+def test_chosen_lang():
+    file_level = "final_project/test.csv"
+    typed_lang_study = ["angielski", "angielski"]
+    assert chosen_lang("angielski")[0] == (file_level)
